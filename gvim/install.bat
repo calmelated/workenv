c107853462f6c97e63004b39a7620fc04264b0ff
Chad Hsu <calmelated@gmail.com>
2013-02-14 11:20:31 -0600

    Add: bundle support for Gvim

--- gvim/install.bat
+++ gvim/install.bat
@@ -1,12 +1,8 @@
 @echo off
 
 cp *.exe C:\
-mv "C:\Program Files\Vim\_vimrc" "C:\Program Files\Vim\_vimrc.bak"
+move /y "C:\Program Files\Vim\_vimrc" "C:\Program Files\Vim\_vimrc.bak"
 cp _vimrc "C:\Program Files\Vim"
 
-echo ""
-echo ""
 echo "Insall completely, please set PATH environment to C:\ !!"
-echo ""
-echo ""
 pause
